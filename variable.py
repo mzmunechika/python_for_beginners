6d9690669da92b458a0facfbadf2e68cce00e0cb
masrenda <tarenadiyaksa@gmail.com>
2023-05-22 10:51:39 +0700

    add: variable.py

---
+++ variable.py
@@ -0,0 +1,9 @@
+# edit by: owner
+x = 5
+y = "John"
+print(x)
+print(y)
+
+# ==============================
+# edit by: ISI_NAMAMU_DI_SINI
+# tugas: ikuti kodingan di atas dengan value (nilai) yang berbeda, kemudian jalankan
