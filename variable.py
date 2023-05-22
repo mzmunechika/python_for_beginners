b38f23524a419c387450f870e5833415d23d9a91
Manap112 <hanifmaulanaarrasyid@gmail.com>
2023-05-22 12:23:52 +0700

    tugas selesai dikerjakan

--- variable.py
+++ variable.py
@@ -7,3 +7,8 @@ print(y)
 # ==============================
 # edit by: ISI_NAMAMU_DI_SINI
 # tugas: ikuti kodingan di atas dengan value (nilai) yang berbeda, kemudian jalankan
+
+x = 6
+y = 8
+print(x)
+print(y)
