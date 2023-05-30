1dc9648412c616b5abf0bcdc8f587c369545b189
etherious098 <rivaldogeovany@gmail.com>
2023-05-30 18:39:47 +0700

    jawaban 3 day 1

---
+++ day_1/your_code/answer_3.py
@@ -0,0 +1,40 @@
+# nomor 1
+# Variabel adalah sebuah nama yang digunakan untuk menyimpan nilai atau data dalam program. Variabel memungkinkan kita untuk menyimpan dan mengakses data dengan mudah. Di Python, variabel tidak perlu dideklarasikan secara eksplisit sebelum digunakan. Kita bisa langsung membuat variabel dan memberikan nilai kepadanya menggunakan operator assignment.
+
+# nomor 2
+# Operator assignment (=) digunakan untuk memberikan nilai ke variabel. Nilai yang diberikan dapat berupa angka, string, atau tipe data lainnya.
+
+# nomor 3
+x = 10
+
+# nomor 4
+name = "Alice"
+# kurang paham aku mas
+
+# nomor 5
+# mungkin akan error. karena Tidak diperbolehkan menggunakan kata-kata kunci (keyword) yang sudah ada dalam Python sebagai nama variabel.
+
+# nomor 6
+# artinya huruf besar dan huruf kecil dianggap berbeda.
+
+# nomor 7
+# Melalui penggunaan variabel dan assignment, kita dapat mengubah nilai variabel sepanjang program berjalan
+
+# nomor 8
+x = 4
+y = 3
+print(x*y+2)
+
+# nomor 9
+q = 5
+w = "3"
+
+hasil = str(q) + w
+
+print(hasil)
+
+# nomor 10
+# seperti ini
+name = "John"
+greeting = "Hi, " + name + "!"
+print(greeting)
